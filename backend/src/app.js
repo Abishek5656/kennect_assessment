21d0d9830623db6a359914178ddec2becf0d49fa
Abishek Varma <abishek@entrolabs.com>
2025-02-26 23:03:30 +0530

    changes in app.js file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from 'cookie-parser';
 
 const app = express()
 
@@ -7,9 +8,17 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }))
 
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
+
+//import route
+import userRoute from "../src/routes/user.router.js"
+
+//routes declaration
+app.use("/api/v1/user",userRoute )
 
 export { app }
