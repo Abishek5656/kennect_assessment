845821433e27ac222f19b95178ed40aa1cdf7994
Abishek Varma <abishek@entrolabs.com>
2025-02-26 23:01:15 +0530

    updated helper function

---
+++ backend/src/utils/helper.js
@@ -0,0 +1,14 @@
+import bcrypt from "bcryptjs";
+
+export const generateHassedPassword = async (password) => {
+    let salt = bcrypt.genSaltSync(10);
+    let hashedPassword =  bcrypt.hashSync(password, salt);
+  
+    return hashedPassword;
+  };
+  
+  export const compareHassedPassword = async (password, hassedPassword) => {
+    const comparePassword = await bcrypt.compare(password, hassedPassword); 
+    return comparePassword;
+  };
+  
