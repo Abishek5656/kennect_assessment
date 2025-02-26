117d76e6b0823769a7e5ac0d6d07a74f6433abf9
Abishek Varma <abishek@entrolabs.com>
2025-02-27 01:03:33 +0530

    added protectedroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { Suspense } from "react";
-
-
+import ProtectedRoute from "./pages/ProtectedRoute.jsx"; 
 
 function App() {
-
-  
+  const user = false;
 
   return (
     <>
-     <Suspense fallback={<div>Loading...</div>}>
-     <BrowserRouter>
-        <Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Routes>
 
-          <Route path="/login" element={<>Login Page</>} />
-          <Route path="/signUp" element={<>Sign up Page</>} />
-          <Route path="/" element={<>HomePage</>} />
-
-        </Routes>
-      </BrowserRouter>
-     </Suspense>
-     </>
-  )
+          <Route  path="/" element={
+                <ProtectedRoute user={user}>
+                  <>Home Page (Protected)</>
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<>Login Page</>} />
+            <Route path="/signUp" element={<>Sign up Page</>} />
+          
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </>
+  );
 }
 
-export default App
+export default App;
