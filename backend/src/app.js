dfc869e21f462a119d2fcf8982014b60a3076e3f
Abishek Varma <abishek@entrolabs.com>
2025-02-27 00:23:35 +0530

    added post declaration

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,8 +17,10 @@ app.use(cookieParser())
 
 //import route
 import userRoute from "../src/routes/user.router.js"
+import postRoute from "../src/routes/post.router.js"
 
 //routes declaration
-app.use("/api/v1/user",userRoute )
+app.use("/api/v1/user",userRoute)
+app.use("/api/v1/post", postRoute)
 
 export { app }
