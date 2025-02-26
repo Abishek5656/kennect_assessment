f69d3a407b42dd2f7a6f04b67c6add55bb9929ec
Abishek Varma <abishek@entrolabs.com>
2025-02-27 01:03:07 +0530

    updated userslice

--- frontend/src/store/slice/userSlice.js
+++ frontend/src/store/slice/userSlice.js
@@ -1,23 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-    username =  "",
-    password = "",
-];
+const initialState = {
+    username: "",
+    password: "",
+};
 
 const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
         userDetails: (state, action) => {
-            state.username = action.username,
-            state.username = action.password,
+            state.username = action.payload.username;
+            state.password = action.payload.password;
         },
     },
 });
 
-
-export const selectAllmenu = (state) => state.user;
+export const selectUser = (state) => state.user; 
 
 export const { userDetails } = userSlice.actions;
 
