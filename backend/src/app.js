06e5c7d0ca6efd73bdae960cba1e4044c4b09786
Abishek Varma <abishek@entrolabs.com>
2025-02-27 22:33:28 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,15 @@ import cookieParser from 'cookie-parser';
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-}))
+    origin: "http://localhost:5173",
+    credentials: true  
+}));
 
 
-app.use(express.json({limit: "16kb"}))
+app.use(cookieParser())
+app.use(express.json())
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
-
 
 //import route
 import userRoute from "../src/routes/user.router.js"
@@ -23,5 +23,6 @@ import commentRoute from "../src/routes/comment.route.js";
 //routes declaration
 app.use("/api/v1/user",userRoute)
 app.use("/api/v1/post", postRoute)
+app.use("/api/v1/comment",commentRoute )
 
 export { app }
