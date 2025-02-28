e180fbefc9323baed69643e582aa14f311702792
Abishek Varma <abishek@entrolabs.com>
2025-02-28 13:32:37 +0530

    updated app and main js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,48 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { Suspense, useEffect } from "react";
-import ProtectedRoute from "./pages/ProtectedRoute.jsx"; 
-import {useSelector} from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, { Suspense } from "react";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+import { useSelector } from "react-redux";
 
-import { user } from "./store/slice/userSlice.js"
+import { user } from "./store/slice/userSlice.js";
 
 const SignUp = React.lazy(() => import("./pages/SignUp.jsx"));
 const Login = React.lazy(() => import("./pages/Login.jsx"));
-const Homelayout = React.lazy(() => import("./pages/Homelayout.jsx"))
-
+const Homelayout = React.lazy(() => import("./pages/Homelayout.jsx"));
 
 function App() {
-
   const userDetails = useSelector(user);
-
+  const token = localStorage.getItem("token"); 
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <BrowserRouter>
-          <Routes>
-          <Route  path="/" element={
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              !token ? (
+                <Navigate to="/login" />
+              ) : (
                 <ProtectedRoute user={userDetails}>
-                 <Homelayout />
+                  <Homelayout />
                 </ProtectedRoute>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signUp" element={<SignUp />} />
-          
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-    </>
+              )
+            }
+          />
+
+          {/* Login Route */}
+          <Route
+            path="/login"
+            element={token ? <Navigate to="/" /> : <Login />}
+          />
+
+          {/* SignUp Route */}
+          <Route
+            path="/signUp"
+            element={token ? <Navigate to="/" /> : <SignUp />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
