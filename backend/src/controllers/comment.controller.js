f3b91e4c7fcda54f23809eb8ccf164a88fe73648
Abishek Varma <abishek@entrolabs.com>
2025-02-28 13:30:07 +0530

    commen done

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,16 +1,16 @@
 import { Comment } from "../models/comment.model.js";
 import { Post } from "../models/post.model.js";
+import mongoose from "mongoose";
 
 const getCommentsByPost = async (req, res) => {
 
   const { postId } = req.params;
   try {
 
-    console.log(postId)
-
+  
     const postComments = await Comment.aggregate([
       {
-        $match: {postId: ObjectId(postId)}
+        $match: { postId: new mongoose.Types.ObjectId(postId) }
       },
       {
         $lookup: {
@@ -21,19 +21,30 @@ const getCommentsByPost = async (req, res) => {
         }
       },
       {
+        $addFields: {
+          user: { $arrayElemAt: ["$userDetails", 0] }
+        }
+      },
+      {
         $project: {
           content: 1,
-          "userDetails.username":1
+          createdAt:1,
+          username:"$user.username"
         }
       }
     ])
 
+    return res.status(200).json({
+      success: true,
+      message: "",
+      data: postComments,
+    });
 
     console.log("postComments->",postComments )
 
   } catch (error) {
     return res.status(500).json({
-      status: false,
+      success: false,
       message: "Internal Server Error",
       data: error.message,
     });
@@ -58,7 +69,6 @@ const addComment = async (req, res) => {
       });
     }
 
-    // Create a new comment
     const newComment = await Comment.create({
       content,
       postId,
@@ -70,61 +80,18 @@ const addComment = async (req, res) => {
     });
 
     return res.status(201).json({
-      status: true,
+      success: true,
       message: "Comment added successfully",
       data: newComment,
     });
   } catch (error) {
     return res.status(500).json({
-      status: false,
+      success: false,
       message: "Internal Server Error",
       data: error.message,
     });
   }
 };
 
-const removeComment = async (req, res) => {
-  const { _id } = req.admin;
-  const { commentId } = req.params;
 
-  console.log("commentId->", commentId);
-
-  try {
-    const existingComment = await Comment.findById(commentId);
-
-    const postDetails = await Post.findById(existingComment?.postId);
-
-    if (_id.toString() === postDetails.owner.toString()) {
-      console.log("true");
-    }
-
-    if (postDetails?.owner.toString() !== _id.toString()) {
-      return res.status(400).json({
-        status: false,
-        message: "You do not have permission to delete this comment",
-        data: "",
-      });
-    }
-
-    postDetails.comments = postDetails.comments.filter(
-      (comment) => comment.toString() !== commentId.toString()
-    );
-    await postDetails.save();
-
-    await Comment.findByIdAndDelete(commentId);
-
-    return res.status(200).json({
-      status: true,
-      message: "Comment removed successfully",
-      data: "",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: false,
-      message: "Internal Server Error",
-      data: error.message,
-    });
-  }
-};
-
-export { addComment, removeComment, getCommentsByPost };
+export { addComment, getCommentsByPost };
--- backend/src/routes/comment.route.js
+++ backend/src/routes/comment.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {verifyJWT } from "../middleware/auth.middleware.js"
-import  {addComment, removeComment, getCommentsByPost} from "../controllers/comment.controller.js"
+import  {addComment,getCommentsByPost} from "../controllers/comment.controller.js"
 
 const router = Router();
 
@@ -10,14 +10,6 @@ router.use(verifyJWT);
 router.get("/commentpost/:postId",getCommentsByPost);
 
 router.post("/createComment/:postId",addComment);
-
-router.delete("/:commentId", removeComment);
-
-
-
-
-
-
 
 
 export default router;
